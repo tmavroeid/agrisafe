24041eccf859d089035c553211e0f2870556d429
NikosFotiadis <nikosfotiadis0@gmail.com>
2024-07-13 14:02:41 +0300

    feat: add web3modal

--- frontend/src/app/provider/create-insurance/page.tsx
+++ frontend/src/app/provider/create-insurance/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
-import SelectGroupOne from "@/components/SelectGroup/SelectGroupOne";
+import InsuranceTypeSelect from "@/components/InsuranceComponents/InsuranceTypeSelect";
 import DatePickerOne from "@/components/FormElements/DatePicker/DatePickerOne";
 
 export default function CreateInsurance() {
@@ -22,14 +22,7 @@ export default function CreateInsurance() {
               <div className="p-6.5">
                 <div className="mb-4.5 flex flex-col gap-6 xl:flex-row">
                   <div className="w-full xl:w-1/2">
-                    <label className="mb-3 block text-sm font-medium text-black dark:text-white">
-                      Insurance type
-                    </label>
-                    <input
-                      type="text"
-                      placeholder="Enter your first name"
-                      className="w-full rounded border-[1.5px] border-stroke bg-transparent px-5 py-3 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                    />
+                    <InsuranceTypeSelect/>
                   </div>
                 </div>
 
--- frontend/src/components/Layouts/DefaultLayout.tsx
+++ frontend/src/components/Layouts/DefaultLayout.tsx
@@ -1,7 +1,51 @@
 "use client";
 import React, { useState, ReactNode } from "react";
 import Sidebar from "@/components/Sidebar";
-import Header from "@/components/Header";
+import Header from "@/components/InsuranceComponents/Header";
+
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+import { mainnet, sepolia } from 'wagmi/chains'
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { cookieStorage, createStorage, State, WagmiProvider, cookieToInitialState } from 'wagmi'
+import { headers } from 'next/headers'
+
+// Get projectId from https://cloud.walletconnect.com
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+const metadata = {
+  name: 'Web3Modal',
+  description: 'Web3Modal Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+// Create wagmiConfig
+const chains = [mainnet, sepolia] as const
+export const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+  ssr: true,
+  storage: createStorage({
+    storage: cookieStorage
+  }),
+})
+
+// Setup queryClient
+const queryClient = new QueryClient()
+
+if (!projectId) throw new Error('Project ID is not defined')
+
+// Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
 
 export default function DefaultLayout({
   children,
@@ -9,31 +53,37 @@ export default function DefaultLayout({
   children: React.ReactNode;
 }) {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  // const initialState = cookieToInitialState(config, headers().get('cookie'))
+
   return (
     <>
       {/* <!-- ===== Page Wrapper Start ===== --> */}
-      <div className="flex">
-        {/* <!-- ===== Sidebar Start ===== --> */}
-        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* <!-- ===== Sidebar End ===== --> */}
+    <WagmiProvider config={config}>
+        <QueryClientProvider client={queryClient}>
+          <div className="flex">
+            {/* <!-- ===== Sidebar Start ===== --> */}
+            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            {/* <!-- ===== Sidebar End ===== --> */}
 
-        {/* <!-- ===== Content Area Start ===== --> */}
-        <div className="relative flex flex-1 flex-col lg:ml-72.5">
-          {/* <!-- ===== Header Start ===== --> */}
-          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-          {/* <!-- ===== Header End ===== --> */}
+            {/* <!-- ===== Content Area Start ===== --> */}
+            <div className="relative flex flex-1 flex-col lg:ml-72.5">
+              {/* <!-- ===== Header Start ===== --> */}
+              <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+              {/* <!-- ===== Header End ===== --> */}
 
-          {/* <!-- ===== Main Content Start ===== --> */}
-          <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-              {children}
+              {/* <!-- ===== Main Content Start ===== --> */}
+              <main>
+                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+                  {children}
+                </div>
+              </main>
+              {/* <!-- ===== Main Content End ===== --> */}
             </div>
-          </main>
-          {/* <!-- ===== Main Content End ===== --> */}
-        </div>
-        {/* <!-- ===== Content Area End ===== --> */}
-      </div>
-      {/* <!-- ===== Page Wrapper End ===== --> */}
+            {/* <!-- ===== Content Area End ===== --> */}
+          </div>
+          {/* <!-- ===== Page Wrapper End ===== --> */}
+        </QueryClientProvider>
+      </WagmiProvider>
     </>
   );
 }
